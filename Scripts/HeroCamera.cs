20f387207b34f623b4c77da9792a5a212704bf80
Michael Morano <midautumnrain@yandex.com>
2018-12-16 23:11:15 -0500

    Added scripts and prefab
    Scripts may or may not  need to be reapplied to prefab object.

---
+++ Scripts/HeroCamera.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroCamera : MonoBehaviour
+{
+    //GameObject player;
+    //HeroController heroController;
+    
+    public float sensitivity;
+    public float height;
+
+    public bool isThirdPerson;
+    public float distance = 3;
+
+    public float yaw;
+    private float pitch;
+
+    //Legacy camera is refering to the style of control used in MMORPGS such as Final Fantasy XIV
+    public bool isLegacy = false;
+    private int camSynced = 1;
+    private int camUnsynced = 0;
+    
+     
+    void Start()
+    {
+        //player = GetComponentInParent<GameObject>();
+        //heroController = GetComponentInParent<HeroController>();
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+    
+    void Update()
+    {
+        if (isLegacy == false)
+        {
+            pitch += Input.GetAxis("Mouse Y");
+            yaw += Input.GetAxis("Mouse X");
+            gameObject.transform.rotation = Quaternion.Euler(new Vector3(-pitch, yaw, 0) * sensitivity);
+        }
+        if (isLegacy && Input.GetMouseButton(camSynced))
+        {
+            
+        }
+        if (isLegacy && Input.GetMouseButton(camUnsynced))
+        {
+
+        }
+    }
+}
